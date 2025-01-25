2768ed5e070c070d5a2449f4d4ab1c2c222de211
Gabriel V <gabriel8vicente@gmail.com>
2025-01-24 21:45:52 -0300

    atualização_1

--- Tarefa_3_embarcatech.c
+++ Tarefa_3_embarcatech.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pio_matrix.pio.h"
+#include "hardware/pio.h"
 
 // Definição do número de linhas e colunas do teclado matricial
 #define row 4
@@ -36,6 +38,8 @@ void all_led_azul_100();
 void all_led_vermelho_80();
 void all_led_verde_50();
 void all_led_branco_20();
+
+
 
 int main()
 {
@@ -87,6 +91,29 @@ int main()
 
 }
 
+}
+void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
+
+    for (int16_t i = 0; i < NUM_PIXELS; i++) {
+        if (i%2==0)
+        {
+            valor_led = matrix_rgb(desenho[24-i], r=0.0, g=0.0);
+            pio_sm_put_blocking(pio, sm, valor_led);
+
+        }else{
+            valor_led = matrix_rgb(b=0.0, desenho[24-i], g=0.0);
+            pio_sm_put_blocking(pio, sm, valor_led);
+        }
+    }
+    imprimir_binario(valor_led);
+}
+uint32_t matrix_rgb(double b, double r, double g)
+{
+  unsigned char R, G, B;
+  R = r * 255;
+  G = g * 255;
+  B = b * 255;
+  return (G << 24) | (R << 16) | (B << 8);
 }
 
 void inicializar_pinos(){
